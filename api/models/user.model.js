1264be638684631e72553c4e7ac993406421cf1a
Bhavy <bhavypdevani31@gmail.com>
2023-12-31 18:47:20 +0530

    set the profile img through OAuth google and make profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,7 +15,11 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-  }
+  },
+  avatar: {
+    type: String,
+    default: "https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg"
+  },
 }, {
   timestamps: true
 })
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 const Header = () => {
+  const { currentUser } = useSelector(state => state.user);
+
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-2 h-14' >
@@ -22,14 +26,20 @@ const Header = () => {
           </Link>
           <Link to='about'>
             <li className='hidden  sm:inline hover:underline text-slate-700'>About</li>
+
           </Link>
-          <Link to='sign-in'>
-            <li className='hidden  sm:inline hover:underline text-slate-700'>Sign In</li>
+          <Link to='/profile'>
+            {
+              currentUser ? (
+                <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" />
+              ) :
+                (<li className='hidden  sm:inline hover:underline text-slate-700'>Sign In</li>
+                )}
           </Link>
         </ul>
       </div>
 
-    </header>
+    </header >
   )
 }
 
