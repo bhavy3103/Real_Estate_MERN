82de01ff299d1ef437b9a72d7775dbeada0ca657
Bhavy <bhavypdevani31@gmail.com>
2024-01-01 20:55:25 +0530

    image container opend and select profile image

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,28 @@
 // import React from 'react'
-import { current } from "@reduxjs/toolkit";
+// import { current } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux"
+import { useRef, useState } from "react";
+
+
+//firebase storage
+// allow read;
+// allow write :if
+// request.resource.size < 2*1024*1024 &&
+// request.resource.contentType.matches('image/.*')
+
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const { file, setFile } = useState('undefined');
+  console.log("img", file);
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.avatar} alt="Profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
+        <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
+        <img onClick={() => fileRef.current.click()} src={currentUser.avatar} alt="Profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
         <input type="text" placeholder="Username" id="username" className="border p-3 rounded-lg" />
         <input type="email" placeholder="email" id="email" className="border p-3 rounded-lg" />
         <input type="text" placeholder="password" id="password" className="border p-3 rounded-lg" />
