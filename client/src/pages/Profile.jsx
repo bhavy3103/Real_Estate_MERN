fbfca6ed39d206e26ec3c1397ebed0a06ae3b834
Bhavy <bhavypdevani31@gmail.com>
2024-01-02 21:22:52 +0530

    Add image functionality completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,7 @@
-// import React from 'react'
-// import { current } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux"
-import { useRef, useState } from "react";
-
+import { useRef, useState, useEffect } from "react";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from '../firebase';
 
 //firebase storage
 // allow read;
@@ -10,19 +9,65 @@ import { useRef, useState } from "react";
 // request.resource.size < 2*1024*1024 &&
 // request.resource.contentType.matches('image/.*')
 
-
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
   const fileRef = useRef(null);
-  const { file, setFile } = useState('undefined');
-  console.log("img", file);
+  const [file, setFile] = useState(undefined);
+  // console.log(file);
+  const [filePrec, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  // console.log(formData);
+  // console.log((filePrec));
+  // console.log((fileUploadError));
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // console.log('Upload is ' + progress + '% done');
+        setFilePerc((Math.round(progress)));
+        // console.log(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        })
+      }
+    );
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className="flex flex-col gap-4">
         <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
-        <img onClick={() => fileRef.current.click()} src={currentUser.avatar} alt="Profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
+        <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="Profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
+        <p className="text-sm self-center">
+          {fileUploadError ?
+            (<span className="text-red-700">Error Upload Image(Image must be less than 2 mb</span>) :
+            filePrec > 0 && filePrec < 100 ? (
+              <span className="text-slate-700">{`Uploading ${filePrec}%`}</span>
+            ) :
+              filePrec === 100 ? (
+                <span className="text-green-700">Successfully Uploaded</span>
+              ) : ''
+          }
+        </p>
         <input type="text" placeholder="Username" id="username" className="border p-3 rounded-lg" />
         <input type="email" placeholder="email" id="email" className="border p-3 rounded-lg" />
         <input type="text" placeholder="password" id="password" className="border p-3 rounded-lg" />
